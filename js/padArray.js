82c0ad4e773bed975e03128842b8897c32403d41
Dennis Corral <dennis.corral@outlook.com>
2022-05-29 11:49:40 -0400

    working solution

--- js/padArray.js
+++ js/padArray.js
@@ -1,4 +1,24 @@
 // REMEMBER TO PSEUDOCODE
-const pad = (array, minSize, value=null) => {
-
+const pad = (array, minSize, padword) => {
+//for loop to iterate through array and add to reach minsize
+    let finalArr = []
+    if(padword != undefined){
+        while (finalArr.length < minSize){
+            finalArr.push(padword)
+            console.log(finalArr)
+        }
+    }else{
+        while (finalArr.length < minSize){
+            finalArr.push(null)
+            console.log(finalArr)  
+        }
+    }
+    for(let i = 0; i < array.length; i++){
+        if(array[i] != finalArr[i]){
+            finalArr[i] = array[i]
+        }
+    }
+    return finalArr        
 }
+
+console.log(pad([1,2,3,4,5,6,7,8,9,10], 30, 'cat'))
