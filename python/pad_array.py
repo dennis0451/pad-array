f643118f76ab1408e97135e394304f9f3e06529b
Dennis Corral <dennis.corral@outlook.com>
2022-05-30 17:01:47 -0400

    working solution

--- python/pad_array.py
+++ python/pad_array.py
@@ -1,2 +1,21 @@
 #REMEMBER TO PSEUDOCODE
+from secrets import randbelow
+from typing import final
+
+
 def pad(array, min_size, value = None):
+    final_List = []
+    
+    if value == None:
+        while len(final_List) != min_size:
+                final_List.append(None)
+    else:
+        while len(final_List) != min_size:
+                final_List.append(value) 
+    for i in range(0,len(array)):
+        if array[i] != final_List[i]:
+            final_List[i] = array[i]  
+
+    return final_List      
+
+print(pad([1,2,3,4,5],14,"frog"))
